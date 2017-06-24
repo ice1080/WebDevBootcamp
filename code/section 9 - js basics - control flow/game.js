6f739aa3f43e3816b4916081fbfd3f481e12c88d
ihudson <ice1080@gmail.com>
2017-06-24 15:53:33 -0600

    Adding section 9

---
+++ code/section 9 - js basics - control flow/game.js
@@ -0,0 +1,18 @@
+//create secretNumber
+var secretNumber = 4;
+
+//ask user for guess
+var guess = Number(prompt("guess a number"));
+
+//check guess
+if(guess === secretNumber) {
+	alert("You got it right");
+}
+
+else if(guess > secretNumber) {
+	alert("Too high. Guess again");
+}
+
+else {
+	alert("Too low. Guess again");
+}
