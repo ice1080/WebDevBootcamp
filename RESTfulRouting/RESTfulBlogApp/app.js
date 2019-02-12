688bfdc24bcaaa77734def920d299f42b9fc7361
Isaac Hudson <isaac.hudson@icd-tech.com>
2019-02-12 09:18:06 -0700

    more stuff in restful routing

--- RESTfulRouting/RESTfulBlogApp/app.js
+++ RESTfulRouting/RESTfulBlogApp/app.js
@@ -30,12 +30,41 @@ app.get('/', function(req, res) {
   res.redirect('/blogs');
 })
 
+// INDEX ROUTE
 app.get('/blogs', function(req, res) {
   Blog.find({}, function(err, blogs) {
     if (err) {
       console.error('error: ', err);
     } else {
       res.render('index', {blogs: blogs});
+    }
+  });
+});
+
+// NEW ROUTE
+app.get('/blogs/new', function(req, res) {
+  res.render('new');
+});
+
+// CREATE ROUTE
+app.post('/blogs', function(req, res) {
+  Blog.create(req.body.blog, function(err, newBlog) {
+    if (err) {
+      res.render('new');
+    } else {
+      res.redirect('/blogs');
+    }
+  })
+});
+
+
+// SHOW ROUTE
+app.get('/blogs/:id', function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if (err) {
+      res.redirect('/blogs');
+    } else {
+      res.render('show', {blog: foundBlog});
     }
   });
 });
