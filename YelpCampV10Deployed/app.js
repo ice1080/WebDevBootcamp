6dd25a19b3488fca53f1bb7db41f17fb6fa4e154
ihudson <ice1080@gmail.com>
2019-03-13 21:19:20 -0600

    finishing up yelp camp deployment module

--- YelpCampV10Deployed/app.js
+++ YelpCampV10Deployed/app.js
@@ -13,7 +13,8 @@ var commentRoutes     = require('./routes/comments'),
     campgroundRoutes  = require('./routes/campgrounds'),
     indexRoutes        = require('./routes/index');
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v10", {useNewUrlParser: true});
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
@@ -47,7 +48,7 @@ app.use('/campgrounds', campgroundRoutes);
 
 
 
-app.listen(3000, process.env.IP, function() {
+app.listen(process.env.PORT, process.env.IP, function() {
   console.log('Yelp Camp server has started');
 });
 
